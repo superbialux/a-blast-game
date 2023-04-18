25254e25410adf691714a28255c750e4d9454622
superbialux <superbia.lux@gmail.com>
2023-04-19 02:51:35 +0700

    Add playable board & tile destruction tests

--- src/store/store.test.js
+++ src/store/store.test.js
@@ -1,8 +1,8 @@
 import { getState, dispatch, initialState } from '.';
 import Vector from '../Math/Vector';
 import { settings } from '../util/constants';
-import { isBoardPlayable } from '../util/tiles';
-import { createTiles } from './actions';
+import { isBoardPlayable, tileBehavior } from '../util/tiles';
+import { createTiles, destroyTiles } from './actions';
 
 describe('common store behavior', () => {
   test('store exists', () => {
@@ -14,10 +14,56 @@ describe('common store behavior', () => {
   });
   test('there are always tiles that can be destroyed', () => {
     let isPlayable = false;
-    for (let i = 0; i < 1000; i += 1) {
+    for (let i = 0; i < 100; i += 1) {
       dispatch(createTiles(new Vector(0, 0), new Vector(50, 50)));
       isPlayable = isBoardPlayable(getState().tiles);
     }
     expect(isPlayable).toBe(true);
+  });
+  test('tiles get destroyed and super tile gets created if threshold is met', () => {
+    const { tiles } = getState();
+    const pairs = [];
+    tiles.forEach((clickedTile, i) => {
+      const neighbors = [clickedTile];
+      let id = 0;
+
+      do {
+        const tile = neighbors[id];
+        const behavior = tileBehavior.normal;
+
+        behavior.indices.forEach((index) => {
+          for (let x = 1; x <= behavior.radius.x; x += 1) {
+            for (let y = 1; y <= behavior.radius.y; y += 1) {
+              const neighbor = tiles.find(
+                ({ indices }) =>
+                  indices &&
+                  indices.isEqual(Vector.add(tile.indices, Vector.mult(index, new Vector(x, y))))
+              );
+
+              if (!neighbor) return;
+              if (tile.type !== neighbor.type) return;
+
+              if (!neighbors.find((t) => t.indices.isEqual(neighbor.indices)))
+                neighbors.push(neighbor);
+            }
+          }
+        });
+        id += 1;
+      } while (neighbors[id]);
+
+      pairs[i] = neighbors;
+    });
+
+    pairs.sort((a, b) => b.length - a.length);
+    const activePair = pairs[0];
+    dispatch(destroyTiles(activePair[0]));
+
+    if (activePair.length > settings.superTileThreshold) {
+      expect(getState().tiles.filter(({ toDestroy }) => toDestroy)).toHaveLength(
+        activePair.length - 1
+      );
+    } else {
+      expect(getState().tiles.filter(({ toDestroy }) => toDestroy)).toHaveLength(activePair.length);
+    }
   });
 });
--- src/util/constants.js
+++ src/util/constants.js
@@ -5,9 +5,9 @@ const types = ['blue', 'green', 'purple', 'yellow', 'red'];
 const settings = {
   aspectRatio: 16 / 10,
   fps: 30,
-  size: new Vector(2, 2),
+  size: new Vector(8, 8),
   superTileThreshold: 3, // if destroyed more than x, activate super tile
-  minTiles: 3, // if surrounding tiles of the same type is less than 2 then do nothing
+  minTiles: 2, // if surrounding tiles of the same type is less than 2 then do nothing
 };
 
 export { types, settings };
